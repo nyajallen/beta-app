67887fe81df893c16bd0ab1ccb396f6a0214cd11
jeantyg13@students.ecu.edu <jeantyg13@students.ecu.edu>
2021-03-02 14:57:21 -0500

    New java jars, gradle dependencies, added test class.

---
+++ src/Main_UI/CWA_UI/app/src/test/java/child/wellness/app/FirstTest.java
@@ -0,0 +1,66 @@
+package child.wellness.app;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+
+
+public class FirstTest {
+
+    WebDriver driver;
+
+    @Before
+    public void setUp() throws MalformedURLException {
+        // Created object of DesiredCapabilities class.
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+
+        // Set android deviceName desired capability. Set your device name.
+        capabilities.setCapability("deviceName", "sdk_gphone_x86_arm");
+
+        // Set android VERSION desired capability. Set your mobile device's OS version.
+        capabilities.setCapability(CapabilityType.VERSION, "11.0");
+
+        // Set android platformName desired capability. It's Android in our case here.
+        capabilities.setCapability("platformName", "Android");
+
+        // Set android appPackage desired capability. It is
+        // com.android.calculator2 for calculator application.
+        // Set your application's appPackage if you are using any other app.
+        capabilities.setCapability("appPackage", "child.wellness.app");
+
+        // Set android appActivity desired capability. It is
+        // com.android.calculator2.Calculator for calculator application.
+        // Set your application's appPackage if you are using any other app.
+        capabilities.setCapability("appActivity", "child.wellness.app.Login");
+
+        // Created object of RemoteWebDriver will all set capabilities.
+        // Set appium server address and port number in URL string.
+        // It will launch calculator app in android device.
+        driver = new RemoteWebDriver(new URL("http://0.0.0.0:4723/wd/hub"), capabilities);
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+
+
+    }
+
+    @Test
+    public void TurnOnApplication() {
+        driver.manage().timeouts().implicitlyWait(300,TimeUnit.SECONDS);
+    }
+
+    @After
+    public void End() {
+        driver.quit();
+    }
+}
