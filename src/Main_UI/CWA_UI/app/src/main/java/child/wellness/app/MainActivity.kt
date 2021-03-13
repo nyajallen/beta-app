e74ba55e24ce120f68f8e557b149e50883133611
ssmerchant89 <merchants18@students.ecu.edu>
2021-03-13 00:51:27 -0500

    added emergency button functionality

--- src/Main_UI/CWA_UI/app/src/main/java/child/wellness/app/MainActivity.kt
+++ src/Main_UI/CWA_UI/app/src/main/java/child/wellness/app/MainActivity.kt
@@ -1,18 +1,31 @@
 package child.wellness.app
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.app.PendingIntent
+import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationManager
+import android.net.Uri
 import android.os.Bundle
+import android.telephony.SmsManager
 import android.view.View
-import android.widget.Button
-import android.widget.ImageButton
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+
 
 class MainActivity : AppCompatActivity() {
-
+    private lateinit var locationManager: LocationManager
+    private lateinit var tvGpsLocation: TextView
+    private val locationPermissionCode = 2
     private lateinit var callButton: Button
     private lateinit var textButton: Button
+    private lateinit var sendButton: Button
+    private lateinit var cancelButton: Button
     private lateinit var emergencyButton: Button
     private lateinit var happyButton: ImageButton
     private lateinit var sadButton: ImageButton
@@ -20,8 +33,65 @@ class MainActivity : AppCompatActivity() {
     private lateinit var sickButton: ImageButton
     private lateinit var hurtButton: ImageButton
     private lateinit var checkInTextView: TextView
+    private lateinit var textInput: EditText
+    private var phoneNumber = "tel:555-555-5555"
 
 
+
+    private fun callParent(): Boolean {
+        val phoneIntent = Intent(Intent.ACTION_CALL);
+        phoneIntent.setData(Uri.parse(phoneNumber));
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            Toast.makeText(this,"Error making call...", Toast.LENGTH_LONG).show()
+            return false;
+        }
+        startActivity(phoneIntent);
+        return true;
+    }
+
+
+
+    private fun sendSMSTextInput() {
+        val scAddress: String? = null
+        val sms: String = textInput.getText().toString()
+        val smsIntent = Intent(Intent.ACTION_SENDTO)
+        smsIntent.setData(Uri.parse(phoneNumber))
+        val sentIntent: PendingIntent? = null
+        val deliveryIntent: PendingIntent? = null
+        val smsManager: SmsManager = SmsManager.getDefault()
+
+        smsManager.sendTextMessage(
+            phoneNumber, scAddress, sms,
+            sentIntent, deliveryIntent)
+    }
+
+    private fun sendSMSEmoticon(string: String):Boolean {
+        val scAddress: String? = null
+        val sms: String = string
+        val smsIntent = Intent(Intent.ACTION_SENDTO)
+        smsIntent.setData(Uri.parse(phoneNumber))
+        val sentIntent: PendingIntent? = null
+        val deliveryIntent: PendingIntent? = null
+        val smsManager: SmsManager = SmsManager.getDefault()
+
+        smsManager.sendTextMessage(
+            phoneNumber, scAddress, sms,
+            sentIntent, deliveryIntent)
+
+        return true;
+    }
+
+
+
+
+    @SuppressLint("MissingPermission")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -34,43 +104,90 @@ class MainActivity : AppCompatActivity() {
         madButton = findViewById((R.id.mad_btn))
         sickButton = findViewById((R.id.sick_btn))
         hurtButton = findViewById((R.id.injured_btn))
+        textInput = findViewById(R.id.text_input)
+        sendButton = findViewById(R.id.send_btn)
+        cancelButton = findViewById(R.id.cancel_btn)
         checkInTextView = findViewById(R.id.check_in)
 
 
 
+
+
         callButton.setOnClickListener { view: View ->
-            //function goes here
+            callParent();
         }
 
         textButton.setOnClickListener { view: View ->
             //function goes here
+            textButton.setVisibility(View.GONE);
+            textInput.setVisibility(View.VISIBLE);
+            sendButton.setVisibility(View.VISIBLE);
+            cancelButton.setVisibility(View.VISIBLE);
             //Start SMS_Messenger activity
-            val intent = Intent(this, SMS_Messenger::class.java)
-            startActivity(intent)
+            //val intent = Intent(this, SMS_Messenger::class.java)
+            //startActivity(intent)
         }
 
-        emergencyButton.setOnClickListener { view: View ->
-            //function goes here
+        cancelButton.setOnClickListener { view: View ->
+            textButton.setVisibility(View.VISIBLE);
+            textInput.setVisibility(View.GONE);
+            sendButton.setVisibility(View.GONE);
+            cancelButton.setVisibility(View.GONE);
         }
+
+        sendButton.setOnClickListener { view: View ->
+            if (textInput.text.isEmpty()) {
+                Toast.makeText(this, "No text entered", Toast.LENGTH_SHORT).show()
+            }
+            else{
+                sendSMSTextInput()
+                textButton.setVisibility(View.VISIBLE);
+                textInput.setVisibility(View.GONE);
+                sendButton.setVisibility(View.GONE);
+                cancelButton.setVisibility(View.GONE);
+                Toast.makeText(this,"Text Successfully Sent...", Toast.LENGTH_LONG).show()
+                textInput.setText("")
+            }
+        }
+
+        emergencyButton.setOnClickListener() { view: View ->
+
+            var lm = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+            val location: Location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+
+            var longitude = location.longitude
+            var latitude = location.latitude
+            sendSMSEmoticon("I HAVE AN EMERGENCY!");
+            sendSMSEmoticon("CHILD'S LOCATION http://www.google.com/maps/place/" + latitude.toString() + "," + longitude.toString());
+
+
+            }
+
+
 
         happyButton.setOnClickListener { view: View ->
-            //function goes here
+            sendSMSEmoticon("I'm Happy!")
+            Toast.makeText(this,"HAPPY EMOTION SENT", Toast.LENGTH_LONG).show()
         }
 
         sadButton.setOnClickListener { view: View ->
-            //function goes here
+            sendSMSEmoticon("I'm Sad!")
+            Toast.makeText(this,"SAD EMOTION SENT", Toast.LENGTH_LONG).show()
         }
 
         madButton.setOnClickListener { view: View ->
-            //function goes here
+            sendSMSEmoticon("I'm Mad!")
+            Toast.makeText(this,"MAD EMOTION SENT", Toast.LENGTH_LONG).show()
         }
 
         sickButton.setOnClickListener { view: View ->
-            //function goes here
+            sendSMSEmoticon("I'm Sick!")
+            Toast.makeText(this,"SICK EMOTION SENT", Toast.LENGTH_LONG).show()
         }
 
         hurtButton.setOnClickListener { view: View ->
-            //function goes here
+            sendSMSEmoticon("I'm Hurt!")
+            Toast.makeText(this,"HURT EMOTION SENT", Toast.LENGTH_LONG).show()
         }
 
         checkInTextView.setOnClickListener { view: View ->
@@ -81,5 +198,5 @@ class MainActivity : AppCompatActivity() {
 
 
 
-        }
     }
+}
